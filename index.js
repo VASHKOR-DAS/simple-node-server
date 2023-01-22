e9256a6a77b45661f65d091d072dca2599e1531b
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-22 22:04:21 +0600

    search query added

--- index.js
+++ index.js
@@ -17,12 +17,22 @@ const users = [
 ];
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    console.log(req.query); // query diye data filter kore
+    const search = req.query.name; // name diye search kora query diye
+    if (req.query.name) {
+        // filter user by query
+        // http://localhost:5000/users?name=joy aikhane name property likhe search dite parbo
+        const filtered = users.filter(usr => usr.name.toLocaleLowerCase().indexOf(search) >= 0);
+        res.send(filtered); // filtered jinish gulo send
+    }
+    else {
+        res.send(users);
+    }
 });
 
 // Client site theke data server a pathabo
 app.post('/users', (req, res) => {
-    console.log('Post API called'); //client site theke hit kore kina ta janar jnno (middle ware)
+    // console.log('Post API called'); //client site theke hit kore kina ta janar jnno (middle ware)
     // console.log(req.body); // client site theke body er moddhe data pathano hoise tai read kora
     const user = req.body;
     user.id = users.length + 1; // users er majhe jotogulo obj ase tar theke 1 besi id
